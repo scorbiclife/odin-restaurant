42d1f72bc4594cc649f47a59769931b7ccb1550e
scorbiclife <scorbiclife+git@gmail.com>
2025-05-01 07:28:53 +0900

    feat(tab/js): implement tab switching on button click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import "./index.css";
-import home from "./home.js"
-import menu from "./menu.js"
+import home from "./home.js";
+import menu from "./menu.js";
 
 const $content = document.getElementById("content");
-$content?.appendChild(menu);
+
+function setPageToHome() {
+  $content?.replaceChildren(home);
+}
+const $home = document.getElementById("home");
+$home?.addEventListener("click", setPageToHome);
+
+const $menu = document.getElementById("menu");
+$menu?.addEventListener("click", function setPageToMenu() {
+  $content?.replaceChildren(menu);
+});
+
+function initPage() {
+  setPageToHome();
+}
+
+initPage();
