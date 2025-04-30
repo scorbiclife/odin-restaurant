b8985a1651e05a1fb797fbea2759192f8445deac
scorbiclife <scorbiclife+git@gmail.com>
2025-05-01 07:15:19 +0900

    feat(js): replace static web page with dynamically generated content with js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./index.css";
+import restaurantImage from "../assets/restaurant.png";
 
-alert("It works!!!");
+function $(tagName, attributes = {}) {
+  return function (...children) {
+    const $element = document.createElement(tagName);
+    for (const a in attributes) {
+      $element.setAttribute(a, attributes[a]);
+    }
+    $element.append(...children);
+    return $element;
+  };
+}
+
+const $content = document.getElementById("content");
+$content?.appendChild(
+  $("div")(
+    $("h1")("Restaurant"),
+    $("img", { src: restaurantImage, alt: "restaurant image" })(),
+    $("p")("this restaurant is wonderful")
+  )
+);
