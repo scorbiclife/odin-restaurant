93e366bbc85bb227a144268f2850d6312a3b5fef
scorbiclife <scorbiclife+git@gmail.com>
2025-05-01 07:35:14 +0900

    feat(styling): stylistic minor touches

--- src/home.js
+++ src/home.js
@@ -3,6 +3,11 @@ import restaurantImage from "../assets/restaurant.png";
 
 export default $("div")(
   $("h1")("Restaurant"),
-  $("img", { src: restaurantImage, alt: "restaurant image" })(),
+  $("img", {
+    src: restaurantImage,
+    alt: "restaurant image",
+    width: 200,
+    height: 200,
+  })(),
   $("p")("this restaurant is wonderful")
 );
